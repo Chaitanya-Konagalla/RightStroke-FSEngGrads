a2201670550a205b743d24dc35a3a541f1073e2d
Konagalla Krishna Chaitanya <52564006+Chaitanya-Konagalla@users.noreply.github.com>
2020-07-25 20:16:59 +0530

    Add files via upload

---
+++ RightStroke_FS_Assignment_3/B.java
@@ -0,0 +1,19 @@
+package p2;
+import p1.A;
+public class B 
+{
+   public static void main(String arg[])
+   {
+	   A obj = new A();
+	   obj.display();
+   }
+}
+
+
+
+/*
+
+Output:
+This is package p1 and class A
+
+*/
