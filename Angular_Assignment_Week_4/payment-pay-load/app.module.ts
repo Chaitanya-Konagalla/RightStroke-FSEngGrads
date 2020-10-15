3b82f1f59bfbd89495da87e6adbbc35e72dc04e4
Konagalla Krishna Chaitanya <52564006+Chaitanya-Konagalla@users.noreply.github.com>
2020-10-15 18:29:47 +0530

    Add files via upload

---
+++ Angular_Assignment_Week_4/payment-pay-load/app.module.ts
@@ -0,0 +1,26 @@
+import { PaymentPayLoadComponent } from './payment-pay-load/payment-pay-load.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PaymentPayLoadComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
